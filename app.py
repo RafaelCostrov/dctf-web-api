b6a3a6c1bbb30ecfc25a05ff2b578eb1e36480b6
Rafael Costrov <inov2@controller-oraculus.com.br>
2025-07-24 11:22:27 -0300

    Criando auxiliar para deixar o código mais reutilizável.

--- app.py
+++ app.py
@@ -1,65 +1,12 @@
 import requests
 import os
-import json
-import base64
+from auxiliar import gerar_arquivo, gerar_data
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from autenticar import retornar_token
-from envio_drive import salvar_drive, buscar_json
+from envio_drive import buscar_json
 
 app = Flask(__name__)
-
-
-def gerar_data(cnpj, id_sistema, id_servico, dados):
-    json_data = {
-        "contratante": {
-            "numero": "15435766000176",
-            "tipo": 2
-        },
-        "autorPedidoDados": {
-            "numero": "15435766000176",
-            "tipo": 2
-        },
-        "contribuinte": {
-            "numero": f"{cnpj}",
-            "tipo": 2
-        },
-        "pedidoDados": {
-            "idSistema": id_sistema,
-            "idServico": id_servico,
-            "versaoSistema": "1.0",
-            "dados": dados
-        }
-    }
-    return json_data
-
-
-def verificar_json(json):
-    codigo_sem_procuracao = "[AcessoNegado-ICGERENCIADOR-022]"
-    sem_procuracao = any(mensagem.get("codigo") ==
-                         codigo_sem_procuracao for mensagem in json.get("mensagens", []))
-    if sem_procuracao:
-        return jsonify({"error": "Sem procuração para a Oraculu's."}), 403
-
-    codigo_declaracao_inexistente = "[Aviso-DCTFWEB-MG08]"
-    nao_encontrada = any(mensagem.get("codigo") ==
-                         codigo_declaracao_inexistente for mensagem in json.get("mensagens", []))
-    if nao_encontrada:
-        return jsonify({"error": "Declaração não encontrada."}), 404
-
-    codigo_em_andamento = "[EntradaIncorreta-DCTFWEB-MG10]"
-    em_andamento = any(mensagem.get("codigo") ==
-                       codigo_em_andamento for mensagem in json.get("mensagens", []))
-    if em_andamento:
-        return jsonify({"error": "Declaração em andamento."}), 404
-
-    codigo_mit_em_andamento = "[Aviso-MIT-MSG_0036]"
-    mit_em_andamento = any(mensagem.get("codigo") ==
-                           codigo_mit_em_andamento for mensagem in json.get("mensagens", []))
-    if mit_em_andamento:
-        return jsonify({"error": "Existe uma apuração MIT em andamento."}), 404
-
-    return None
 
 
 @app.route('/')
@@ -69,6 +16,8 @@ def index():
 
 @app.route('/darf', methods=['POST'])
 def gerar_guia_dctfweb():
+    load_dotenv()
+    # Pegando parametros
     senha = request.form.get('senha')
     SENHA_API = os.getenv('SENHA_API')
     if senha != SENHA_API:
@@ -79,117 +28,55 @@ def gerar_guia_dctfweb():
     codigo = request.form.get('codigo')
     if not empresa or not cnpj or not competencia:
         return jsonify({"error": "Parâmetros inválidos"}), 400
-    try:
+    mensagem = "Guia DCTFWeb gerada com sucesso."
 
-        load_dotenv()
-        PASTA_DRIVE_DCTFWEB_DARF = os.getenv('PASTA_DRIVE_DCTFWEB_DARF')
-        ano, mes = competencia.split('-')
-        access_token, jwt = retornar_token()
-        url = 'https://gateway.apiserpro.serpro.gov.br/integra-contador/v1/Emitir'
+    # Gerando dados para a requisição
+    ano, mes = competencia.split('-')
+    dados = f'{{"categoria": "GERAL_MENSAL", "anoPA": "{ano}", "mesPA": "{mes}"}}'
+    data = gerar_data(cnpj, "DCTFWEB", "GERARGUIA31", dados)
+    nome_arquivo = f'DARF - {codigo} - {empresa} - {mes}-{ano}.pdf'
+    url = 'https://gateway.apiserpro.serpro.gov.br/integra-contador/v1/Emitir'
 
-        headers = {
-            'Authorization': f'Bearer {access_token}',
-            'Content-Type': 'application/json',
-            'jwt_token': jwt,
-        }
+    # Pegando pasta do drive para DARF
+    PASTA_DRIVE_DCTFWEB_DARF = os.getenv('PASTA_DRIVE_DCTFWEB_DARF')
 
-        dados = f'{{"categoria": "GERAL_MENSAL", "anoPA": "{ano}", "mesPA": "{mes}"}}'
-        data = gerar_data(cnpj, "DCTFWEB", "GERARGUIA31", dados)
-
-        requisicao = requests.post(
-            url,
-            headers=headers,
-            json=data,
-        )
-
-        resposta = requisicao.json()
-        verificador = verificar_json(resposta)
-        if verificador:
-            return verificador
-        dados_json = resposta.get('dados', {})
-        pdfBase64 = json.loads(dados_json).get('PDFByteArrayBase64')
-        pdf_bytes = base64.b64decode(pdfBase64)
-        nome_arquivo = f'DARF - {codigo} - {empresa} - {mes}-{ano}.pdf'
-        with open(nome_arquivo, 'wb') as pdf_file:
-            pdf_file.write(pdf_bytes)
-        arquivo_drive = salvar_drive(
-            mes, ano, nome_arquivo, nome_arquivo, PASTA_DRIVE_DCTFWEB_DARF)
-        resultado = {
-            "mensagem": "Guia DCTFWeb gerada com sucesso.",
-            "fileId": arquivo_drive
-        }
-        if os.path.exists(nome_arquivo):
-            os.remove(nome_arquivo)
-        status = resposta.get('status')
-        if status == 200:
-            return jsonify(resultado), 200
-        else:
-            return jsonify({"error": f"Erro ao gerar guia DCTFWeb - {resposta}"}), 500
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    # Chamando função auxiliar
+    resultado_final = gerar_arquivo(
+        empresa, cnpj, competencia, PASTA_DRIVE_DCTFWEB_DARF, url, data, nome_arquivo, mensagem
+    )
+    return resultado_final
 
 
 @app.route('/recibo', methods=['POST'])
 def gerar_recibo_dctfweb():
+    # Pegando parametros
     senha = request.form.get('senha')
     SENHA_API = os.getenv('SENHA_API')
     if senha != SENHA_API:
         return jsonify({"error": "Senha inválida"}), 403
     empresa = request.form.get('empresa')
-    codigo = request.form.get('codigo')
     cnpj = request.form.get('cnpj')
     competencia = request.form.get('competencia')
+    codigo = request.form.get('codigo')
     if not empresa or not cnpj or not competencia:
         return jsonify({"error": "Parâmetros inválidos"}), 400
-    try:
-        load_dotenv()
-        PASTA_DRIVE_DCTFWEB_RECIBO = os.getenv('PASTA_DRIVE_DCTFWEB_RECIBO')
-        ano, mes = competencia.split('-')
-        access_token, jwt = retornar_token()
-        url = 'https://gateway.apiserpro.serpro.gov.br/integra-contador/v1/Consultar'
+    mensagem = "Recibo DCTFWeb gerado com sucesso."
 
-        headers = {
-            'Authorization': f'Bearer {access_token}',
-            'Content-Type': 'application/json',
-            'jwt_token': jwt,
-        }
+    # Gerando dados para a requisição
+    ano, mes = competencia.split('-')
+    dados = f'{{"categoria": "GERAL_MENSAL", "anoPA": "{ano}", "mesPA": "{mes}"}}'
+    data = gerar_data(cnpj, "DCTFWEB", "CONSRECIBO32", dados)
+    nome_arquivo = f'Recibo - {codigo} - {empresa} - {mes}-{ano}.pdf'
+    url = 'https://gateway.apiserpro.serpro.gov.br/integra-contador/v1/Consultar'
 
-        dados = f'{{"categoria": "GERAL_MENSAL", "anoPA": "{ano}", "mesPA": "{mes}"}}'
-        data = gerar_data(cnpj, "DCTFWEB", "CONSRECIBO32", dados)
+    # Pegando pasta do drive para DARF
+    PASTA_DRIVE_DCTFWEB_RECIBO = os.getenv('PASTA_DRIVE_DCTFWEB_RECIBO')
 
-        requisicao = requests.post(
-            url,
-            headers=headers,
-            json=data,
-        )
-
-        resposta = requisicao.json()
-        verificador = verificar_json(resposta)
-        if verificador:
-            return verificador
-        dados_json = resposta.get('dados', {})
-        pdfBase64 = json.loads(dados_json).get('PDFByteArrayBase64')
-        pdf_bytes = base64.b64decode(pdfBase64)
-        nome_arquivo = f'Recibo - {codigo} - {empresa} - {mes}-{ano}.pdf'
-        with open(nome_arquivo, 'wb') as pdf_file:
-            pdf_file.write(pdf_bytes)
-        arquivo_drive = salvar_drive(
-            mes, ano, nome_arquivo, nome_arquivo, PASTA_DRIVE_DCTFWEB_RECIBO)
-        resultado = {
-            "mensagem": "Recibo DCTFWeb gerado com sucesso.",
-            "fileId": arquivo_drive
-        }
-        if os.path.exists(nome_arquivo):
-            os.remove(nome_arquivo)
-        status = resposta.get('status')
-        if status == 200:
-            return jsonify(resultado), 200
-        else:
-            return jsonify({"error": f"Erro ao gerar o recibo DCTFWeb - {resposta}"}), 500
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    # Chamando função auxiliar
+    resultado_final = gerar_arquivo(
+        empresa, cnpj, competencia, PASTA_DRIVE_DCTFWEB_RECIBO, url, data, nome_arquivo, mensagem
+    )
+    return resultado_final
 
 
 @app.route('/mit', methods=['POST'])
---
+++ auxiliar.py
@@ -0,0 +1,117 @@
+from flask import jsonify, request
+from autenticar import retornar_token
+from envio_drive import salvar_drive
+import json
+import os
+import requests
+import base64
+
+
+def gerar_data(cnpj, id_sistema, id_servico, dados):
+    json_data = {
+        "contratante": {
+            "numero": "15435766000176",
+            "tipo": 2
+        },
+        "autorPedidoDados": {
+            "numero": "15435766000176",
+            "tipo": 2
+        },
+        "contribuinte": {
+            "numero": f"{cnpj}",
+            "tipo": 2
+        },
+        "pedidoDados": {
+            "idSistema": id_sistema,
+            "idServico": id_servico,
+            "versaoSistema": "1.0",
+            "dados": dados
+        }
+    }
+    return json_data
+
+
+def gerar_salvar_arquivo(nome_arquivo, pdf_bytes, mes, ano, pasta, mensagem):
+    with open(nome_arquivo, 'wb') as pdf_file:
+        pdf_file.write(pdf_bytes)
+    arquivo_drive = salvar_drive(
+        mes, ano, nome_arquivo, nome_arquivo, pasta)
+    return {
+        "mensagem": mensagem,
+        "fileId": arquivo_drive
+    }
+
+
+def verificar_json(json):
+    codigo_sem_procuracao = "[AcessoNegado-ICGERENCIADOR-022]"
+    sem_procuracao = any(mensagem.get("codigo") ==
+                         codigo_sem_procuracao for mensagem in json.get("mensagens", []))
+    if sem_procuracao:
+        return jsonify({"error": "Sem procuração para a Oraculu's."}), 403
+
+    codigo_declaracao_inexistente = "[Aviso-DCTFWEB-MG08]"
+    nao_encontrada = any(mensagem.get("codigo") ==
+                         codigo_declaracao_inexistente for mensagem in json.get("mensagens", []))
+    if nao_encontrada:
+        return jsonify({"error": "Declaração não encontrada."}), 404
+
+    codigo_em_andamento = "[EntradaIncorreta-DCTFWEB-MG10]"
+    em_andamento = any(mensagem.get("codigo") ==
+                       codigo_em_andamento for mensagem in json.get("mensagens", []))
+    if em_andamento:
+        return jsonify({"error": "Declaração em andamento."}), 404
+
+    codigo_mit_em_andamento = "[Aviso-MIT-MSG_0036]"
+    mit_em_andamento = any(mensagem.get("codigo") ==
+                           codigo_mit_em_andamento for mensagem in json.get("mensagens", []))
+    if mit_em_andamento:
+        return jsonify({"error": "Existe uma apuração MIT em andamento."}), 404
+
+    return None
+
+
+def gerar_arquivo(empresa, cnpj, competencia, pasta, url, data, nome_arquivo, mensagem):
+    if not empresa or not cnpj or not competencia:
+        return jsonify({"error": "Dados incompletos"}), 400
+
+    # try:
+    ano, mes = competencia.split('-')
+    access_token, jwt = retornar_token()
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+        'jwt_token': jwt,
+    }
+
+    requisicao = requests.post(
+        url,
+        headers=headers,
+        json=data,
+    )
+
+    resposta = requisicao.json()
+    verificador = verificar_json(resposta)
+
+    if verificador:
+        return verificador
+
+    dados_json = resposta.get('dados', {})
+    pdfBase64 = json.loads(dados_json).get('PDFByteArrayBase64')
+    pdf_bytes = base64.b64decode(pdfBase64)
+
+    resultado = gerar_salvar_arquivo(
+        nome_arquivo, pdf_bytes, mes, ano, pasta, mensagem
+    )
+
+    if os.path.exists(nome_arquivo):
+        os.remove(nome_arquivo)
+
+    status = resposta.get('status')
+    if status == 200:
+        return jsonify(resultado), 200
+    else:
+        return jsonify({"error": f"Erro ao gerar arquivo - {resposta}"}), 500
+
+    # except Exception as e:
+    #     return jsonify({"error": str(e)}), 500
